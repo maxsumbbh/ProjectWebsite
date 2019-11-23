cd606aded86bb8709ec0ad76c62e1ddc5dac2b71
maxzayess <maxsumbbh073140@gmail.com>
2019-11-23 21:25:50 +0700

    หน้าhome
    header

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,22 +2,49 @@
 
 @section('content')
  
-{{-- //code ใส่รูป --}}
-        <img src="<?php echo asset('assets/img/1.jpg'); ?>" width="100%">
-{{-- //code ใส่รูป   --}}
+
 <div class="header">
-    <a href="#default" class="logo">CompanyLogo</a>
-    <div class="header-right">
-      <a class="active" href="#home">Home</a>
-      <a href="#contact">Contact</a>
-      <a href="#about">About</a>
-    </div>
+  {{-- //code ใส่รูป --}}
+  <img src="<?php echo asset('assets/img/2.png'); ?>" width="100%" height="200px">
+  {{-- //code ใส่รูป   --}}
+</div>
+
+<div class="navbar">
+  <a href="#" class="active">Home</a>
+  <a href="#">Link</a>
+  <a href="#">Link</a>
+  <a href="#" class="right">Link</a>
+</div>
+
+<div class="row">
+  <div class="side">
+    <h2>About Me</h2>
+    <h5>Photo of me:</h5>
+    <div class="fakeimg" style="height:200px;">Image</div>
+    <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+    <h3>More Text</h3>
+    <p>Lorem ipsum dolor sit ame.</p>
+    <div class="fakeimg" style="height:60px;">Image</div><br>
+    <div class="fakeimg" style="height:60px;">Image</div><br>
+    <div class="fakeimg" style="height:60px;">Image</div>
   </div>
-  
-  <br>
-  
-  <div class="footer">
-    <p>Footer</p>
+  <div class="main">
+    <h2>TITLE HEADING</h2>
+    <h5>Title description, Dec 7, 2017</h5>
+    <div class="fakeimg" style="height:200px;">Image</div>
+    <p>Some text..</p>
+    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    <br>
+    <h2>TITLE HEADING</h2>
+    <h5>Title description, Sep 2, 2017</h5>
+    <div class="fakeimg" style="height:200px;">Image</div>
+    <p>Some text..</p>
+    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
   </div>
+</div>
+
+<div class="footer">
+  <h2>Footer</h2>
+</div>
 
 @endsection
